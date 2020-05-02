ea7d11b102981b883e125a4963e2f652aa8e00f6
Khalid Jamal <5aled.gamal231@gmail.com>
2020-05-02 08:33:28 +0200

    New Pages (Register - Login )

--- js/script.js
+++ js/script.js
@@ -8,25 +8,33 @@ videos.forEach((video) => {
   video.addEventListener("mouseover", function () {
     this.play();
     $(this).siblings('img').fadeOut();
+      $(this).siblings('h1').fadeOut();
+      $(this).css({"opacity": "1"});
       $(this).siblings('.video-play').children('span').children('i').removeClass().addClass('fas fa-pause');
   });
 
   video.addEventListener("mouseout", function () {
     this.pause();
+      $(this).css({"opacity": "0"});
       $(this).siblings('img').fadeIn();
+      $(this).siblings('h1').fadeIn();
       $(this).siblings('.video-play').children('span').children('i').removeClass().addClass('fas fa-play');
 
   });
 
   video.addEventListener("touchstart", function () {
     this.play();
+      $(this).css({"opacity": "1"});
       $(this).siblings('img').fadeOut();
+      $(this).siblings('h1').fadeOut();
       $(this).siblings('.video-play').children('span').children('i').addClass('fas fa-pause');
   });
 
   video.addEventListener("touchend", function () {
     this.pause();
+      $(this).css({"opacity": "0"});
       $(this).siblings('img').fadeIn();
+      $(this).siblings('h1').fadeIn();
       $(this).siblings('.video-play').children('span').children('i').addClass('fas fa-play');
   });
 });
