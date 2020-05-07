90b5cba97395fb7ba195ab8e32b13af7837753cc
Khalid Jamal <5aled.gamal231@gmail.com>
2020-05-07 09:30:40 +0200

    New Page ( HOMEPAGE )

--- js/script.js
+++ js/script.js
@@ -5,15 +5,19 @@ new WOW().init();
     //Start Owl
     $('.hero-carousel').owlCarousel({
     loop:true,
-    margin:10,
-    nav:true,
+    margin:30,
+    nav:false,
+    dots: false,
     stagePadding: 50,
+    autoplay:true,
+    autoplayTimeout:1500,
+    autoplayHoverPause:true,
     responsive:{
         0:{
             items:1
         },
         600:{
-            items:3
+            items:2
         },
         1000:{
             items:2
